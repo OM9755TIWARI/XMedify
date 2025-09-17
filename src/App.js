f031f811ba83b007e4a230ea0e919771f3773248
Om Tiwari <omtiwari9755@gmail.com>
2025-09-17 15:01:54 +0530

    adding in github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import DownloadApp from './components/Sections/DownloadApp/DownloadApp';
+import Footer from "../src/components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Sections/FAQs/FAQs.jsx
@@ -0,0 +1,42 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+import faqBanner from "../../../assets/faqs-banner.jpg";
+import CustomizedAccordions from "../../Accordion/Accordion";
+
+
+export default function FAQs() {
+    const faqs = [
+    { question: "Why choose our medical for your family?", answer: "Lorem ipsum..." },
+    { question: "Why we are different from others?", answer: "Lorem ipsum..." },
+    { question: "Trusted & experience senior care & love", answer: "Lorem ipsum..." },
+    { question: "How to get appointment for emergency cases?", answer: "Lorem ipsum..." },
+    ]
+
+    return (
+        <Box py={4}>
+            <Container maxWidth="xl">
+                <Typography color="primary.main" fontWeight={600} textAlign="center">
+                    Get Your Answer
+                </Typography>
+                <Typography textAlign="center" variant="h2" mb={2}>
+                    Frequently Asked Questions
+                </Typography>
+                    <Grid container alignItems="center" spacing={5}>
+                        <Grid item xs={12} md={6}>
+                            <Box component="img" src={faqBanner} width={1} height="auto" />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Box maxWidth={450}>
+                            <CustomizedAccordions data={faqs} />
+                            </Box>
+                        </Grid>
+
+                    </Grid>
+
+
+            </Container>
+        </Box>
+    )
+}
+
+
